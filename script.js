f66123ec300e994e1d889b942bb4026b4131f615
John Doe <hamedhasan422421@gmail.com>
2022-11-23 21:17:25 +0300

    selection all dom element

---
+++ script.js
@@ -0,0 +1,5 @@
+// select dom element
+const counterContainerEl = document.getElementById('counter-container');
+const addCounterEl = document.getElementById('add-counter');
+const resetBtnEl = document.getElementById('reset');
+
