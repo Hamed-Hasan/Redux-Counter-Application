dde343485415b3ce5cf61f39491eb2cd57900951
John Doe <hamedhasan422421@gmail.com>
2022-11-23 21:36:04 +0300

    action creators

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ const resetBtnEl = document.getElementById('reset');
 const getCounterHTMLStringWithId = (id) =>
   `<div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow mb-5" id="counter-${id}"><div id="counter-value-${id}" class="text-2xl font-semibold"></div><div class="flex space-x-3"><button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment-${id}">Increment</button><button id="decrement-${id}" class="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement">Decrement</button></div></div>`
 
-// action creators
+// action identifiers
 const INCREMENT = 'increment'
 const DECREMENT = 'decrement'
 const ADD_COUNTER = 'addCounter'
 const RESET_COUNTER = 'resetCounter'
+
+// action creators 
+const increment = (counter) => {
+    return {
+        type: INCREMENT,
+        payload: counter,
+    }
+}
+const decrement = (counter) => {
+    return {
+        type: INCREMENT,
+        payload: counter,
+    }
+}
